b22add2d814fdd5693e04181b7f103146278ed88
cryptosvinarnik <na4030680@gmail.com>
2023-04-20 17:28:05 +0300

    +15% for gas

--- arbshib.py
+++ arbshib.py
@@ -66,7 +66,7 @@ def claim_tokens(private_key: str):
 
     logger.info(f"Estimated gas: {gas}")
 
-    tx["gas"] = gas
+    tx["gas"] = int(gas * 1.15)
 
     signed_tx = w3.eth.account.sign_transaction(tx, private_key=private_key)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
